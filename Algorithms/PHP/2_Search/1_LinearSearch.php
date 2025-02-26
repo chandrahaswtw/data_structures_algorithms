e629251335adaa62a1ecc4e673e831cf1398f115
BALLEDA CHANDRAHAS <balledachandrahas@BALLEDAs-MacBook-Pro.local>
2025-02-26 17:15:11 +0530

    linear search comments

--- Algorithms/PHP/2_Search/1_LinearSearch.php
+++ Algorithms/PHP/2_Search/1_LinearSearch.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+As the term "linear search" suggests, this method searches for a specific element by iterating through each item in the array. The array does not need to be sorted and can be in any form. Below is the implementation:
+We aim to find the index of a given element by passing the array and the target element as parameters to the function.
+*/
+
 function linearSearch(array $arr, int $num)
 {
   for ($i = 0; $i < count($arr); $i++) {
--- Algorithms/Typescript/2_Search/1_LinearSearch.ts
+++ Algorithms/Typescript/2_Search/1_LinearSearch.ts
@@ -1,3 +1,8 @@
+/*
+As the term "linear search" suggests, this method searches for a specific element by iterating through each item in the array. The array does not need to be sorted and can be in any form. Below is the implementation:
+We aim to find the index of a given element by passing the array and the target element as parameters to the function.
+*/
+
 function linearSearch(arr: number[], num: number) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === num) {
