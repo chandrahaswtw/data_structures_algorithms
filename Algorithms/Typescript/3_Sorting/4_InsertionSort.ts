08629d5b832cd06f37e232e43d35ba4e20459914
BALLEDA CHANDRAHAS <balledachandrahas@BALLEDAs-MacBook-Pro.local>
2025-02-27 19:39:26 +0530

    Insertion and selection sort in TS

--- Algorithms/Typescript/3_Sorting/4_InsertionSort.ts
+++ Algorithms/Typescript/3_Sorting/4_InsertionSort.ts
@@ -1,3 +1,20 @@
 /*
   Explanation video under root -> Algorithms -> Explanation -> 3_Insertion Sort.mp4
 */
+
+function insertionSort(arr: number[]) {
+  let counter = 0;
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[i]) {
+        let temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+let insertionSortedArray = insertionSort([-5, 1001, 181, 900, 5, 46, -190]);
+console.log(insertionSortedArray);
--- Algorithms/Typescript/3_Sorting/5_SelectionSort.ts
+++ Algorithms/Typescript/3_Sorting/5_SelectionSort.ts
@@ -1,3 +1,23 @@
 /*
   Explanation video under root -> Algorithms -> Explanation -> 4_Selection Sort.mp4
 */
+
+function selectionSort(arr: number[]) {
+  for (let i = 0; i < arr.length; i++) {
+    let leastValueIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      leastValueIndex = arr[leastValueIndex] < arr[j] ? leastValueIndex : j;
+      if (i == 2) {
+        console.log(leastValueIndex);
+      }
+    }
+    //Swap here
+    let temp = arr[i];
+    arr[i] = arr[leastValueIndex];
+    arr[leastValueIndex] = temp;
+  }
+  return arr;
+}
+
+let selectionSortedArray = selectionSort([-5, 1001, 181, 900, 5, 46, -190]);
+console.log(selectionSortedArray);
