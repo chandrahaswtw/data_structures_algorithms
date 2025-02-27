84feae4f8a6dec104bfd253b66b748759f45d8c2
BALLEDA CHANDRAHAS <balledachandrahas@BALLEDAs-MacBook-Pro.local>
2025-02-27 17:42:26 +0530

    Quick and merge sort in TS

---
+++ Algorithms/Typescript/3_Sorting/2_QuickSort.ts
@@ -0,0 +1,42 @@
+/*
+We have a pivot element which is normally the first element.
+We split the entire array in 3:
+    - Left array which contains all the elements less than the pivot. 
+    - Right array which contains all the elements greater than the pivot.
+    - Center array which contains the pivot element and all the other elements which are equal to pivot.
+We in turn split the above array (left and right) into simpler pieces till it’s divided into single element array.
+
+Time complexity: 
+-	Best case: O(nlogn)
+-	Worst case: O(n^2)  When we perform on sorted array.
+-	Average case: O(nlogn)
+
+*/
+
+function quickSort(arr: number[]) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let leftArr: number[] = [];
+  let pivotArr: number[] = [];
+  let rightArr: number[] = [];
+
+  for (let i of arr) {
+    if (i < pivot) {
+      leftArr.push(i);
+    } else if (i > pivot) {
+      rightArr.push(i);
+    } else {
+      pivotArr.push(i);
+    }
+  }
+
+  let sortedLeftArr = quickSort(leftArr);
+  let sortedRightArr = quickSort(rightArr);
+  return [...sortedLeftArr, ...pivotArr, ...sortedRightArr];
+}
+
+let quickSortedArray = quickSort([-5, 1001, 181, 900, 5, 46, -190]);
+console.log(quickSortedArray);
---
+++ Algorithms/Typescript/3_Sorting/3_MergeSort.ts
@@ -0,0 +1,68 @@
+/*
+We divide the array into 2 pieces. Until they are reduced to 1 or 2 elements.
+If array has 2 elements we re-arrange them in ascending order within the array.
+And then we start to merge. While merging, we make sure that they are merged in ascending order.
+Merging will happen between 2 arrays which are already in ascending order.
+
+Time complexity: 
+-	In all cases: O(nlogn) is guarenteed 
+*/
+
+function mergeSort(arr: number[]) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  // Rearranging the array with 2 elements in incresing order.
+  if (arr.length == 2) {
+    return arr[0] < arr[1] ? arr : [arr[1], arr[0]];
+  }
+
+  let median = Math.floor(arr.length / 2);
+  let leftArr = mergeSort(arr.slice(0, median));
+  let rightArr = mergeSort(arr.slice(median));
+
+  let sortedArr: number[] = [];
+
+  let leftIndex = 0;
+  let leftMaxIndex = leftArr.length - 1;
+  let rightIndex = 0;
+  let rightMaxIndex = rightArr.length - 1;
+
+  // Infinite loop until one of the leftArr or rightArr exhaust.
+  while (true) {
+    /* 
+     The > (greater than) ensures that one or the array is completely exhausted and all the remaining array elements are greater in value.
+     We don't know which array is exhausted so we are appending both towards the end. And the break the loop.
+    */
+    if (leftIndex > leftMaxIndex || rightIndex > rightMaxIndex) {
+      sortedArr = [
+        ...sortedArr,
+        ...leftArr.slice(leftIndex),
+        ...rightArr.slice(rightIndex),
+      ];
+      break;
+    }
+
+    /*
+     All we are doing below is taking 2 arrays of different number of elements and sorting in ascending order.
+     We took 2 counters and based on what value is being put into sorted array we are increaing the counter of one of them.
+     Since we get 2 ascending arrays in recursion, this appoach make sure to sort everthing in increasing order.
+    */
+
+    let leftElement: number = leftArr[leftIndex];
+    let rightElement: number = rightArr[rightIndex];
+    if (leftElement < rightElement) {
+      sortedArr.push(leftElement);
+      leftIndex++;
+    } else if (leftElement > rightElement) {
+      sortedArr.push(rightElement);
+      rightIndex++;
+    }
+  }
+
+  return sortedArr;
+}
+
+let mergeSortedArray = mergeSort([-5, 1001, 181, 900, 5, 46, -190]);
+console.log(mergeSortedArray);
